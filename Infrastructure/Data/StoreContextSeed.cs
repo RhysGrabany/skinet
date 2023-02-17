e7bdc4c110fb37780ae825473c170ce517f94f11
Rhys Grabany <r.grabany@gmail.com>
2023-02-17 12:59:08 +0000

    End of Section 3 ------------------------------------
    Summary from this section:
    - Utilised a Repository using Interfaces
    - Extended the Product entity to includ Types and Brands - Also created these entities
    - Utilised EF Configuration class to fine tune the database
    - Changed the way database creation is acted by having it be done on app start
    - Added seed data to the database on startup
    - Added code to get Type and Brand from Products
    - Eager loading of navigation properties
    
    So far having fun with the course and I'm looking forward to the introduction of Generics in the next section

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using System.Text.Json;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+
+        public static async Task SeedAsync(StoreContext storeContext)
+        {
+            if (!storeContext.ProductBrand.Any())
+            {
+                var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                storeContext.ProductBrand.AddRange(brands);
+            }
+
+            if (!storeContext.ProductType.Any())
+            {
+                var typeData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+                storeContext.ProductType.AddRange(types);
+            }
+
+            if (!storeContext.Products.Any())
+            {
+                var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                storeContext.Products.AddRange(products);
+            }
+
+
+            if(storeContext.ChangeTracker.HasChanges()) await storeContext.SaveChangesAsync();
+
+        }
+    }
+}

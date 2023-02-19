9955de04d3034e94d502db78fad0b5d8349054ec
Rhys Grabany <r.grabany@gmail.com>
2023-02-19 20:04:06 +0000

    End of Section 8 -------------------- Summary of learning: - Able to get data from the API by using the Http client - Changing the style of the website by using Angular basics - Basics of Observables and TypeScript
    I don't like frontend, bring me back to backend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = []
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products').subscribe({
+      next: response => this.products = response.data, //what to do next
+      error: error => console.error(error), // what to do when error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    })
+  }
+
 }

0f86701ca26347c170884ad66454400d4d86cf88
Rhys Grabany <r.grabany@gmail.com>
2023-02-28 10:27:54 +0000

    End of Section 17 ----------------------- Summary of Learning: - Added validation for identity on client side - Added reactive forms - Created reusable form components - Async validation on email address when signing up with debouncing to decrease amount of API calls

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,25 +5,34 @@ import { PagingHeaderComponent } from './paging-header/paging-header.component';
 import { PagerComponent } from './pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './component/text-input/text-input.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    ReactiveFormsModule,
+    BsDropdownModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
     CarouselModule,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    ReactiveFormsModule,
+    TextInputComponent,
+    BsDropdownModule
   ]
 })
 export class SharedModule { }
